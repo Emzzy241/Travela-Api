3f9afc758cfe8cb738723673e2ddeeff0148a9f1
Emzzy241 <emzzyoluwole@gmail.com>
2024-09-15 16:15:37 +0100

    add Put action to Api

--- TravelApi/Controllers/DestinationController.cs
+++ TravelApi/Controllers/DestinationController.cs
@@ -47,11 +47,10 @@ public class DestinationController: ControllerBase
 
             if (minimumRating > 0)
             {
-                // using the lambda expression inside the Where() method for the minimumRating value
+                
                 querry = querry.Where(entry => entry.OverallRating >= minimumRating);
             }
-            // Converting all destinations that we get from our database into a C# list
-            return await querry.ToListAsync();
+                return await querry.ToListAsync();
         }
 
         [HttpGet("{id}")]
@@ -59,31 +58,54 @@ public class DestinationController: ControllerBase
         {
                 Destination dest = await _db.Destinations.FindAsync(id);
 
-                // TO avoid errors, lets slap in an if statement to actually confirm whether the destination exists
                 if(dest != null)
                 {
                     return dest;
                 }
                
                     return NotFound();
-            // ANother Get() to geta certain Destination based on its id
-
         }
 
-        // Creating a destination
         [HttpPost]
-        public async Task<ActionResult<Destination>> Post(Destination dest)
+        public async Task<ActionResult<Destination>> PostDestination(Destination dest)
         {
-            // NOTE: When creating a new Destination, we should never add in a parameter for id; our databse handles that one for us.. If we give a new destination an id that currently exists sin the databse, we get a 500 error
-            // Remember, we do have an Auto-incrementing integer for our database that helps in giving new destinations an Id
-            // So I gave an Id to a new destination(44), when I now made another Create() but I did not specify an id this time around, my database started counting from 45
+           
             _db.Destinations.Add(dest);
             await _db.SaveChangesAsync();
-            // For our MVC apps, after a CREATE()action is done, we RedirectToAction("Index"),
-            // But for our API projects w will be using a CreatedAtAction() method that takes in 3 parameters; Name of the Action to return to, the id, and the object we are actually creating
             return CreatedAtAction(nameof(GetDestination), new { id = dest.DestinationId}, dest);
         }
 
-        // 
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Destination>> PutDestination(int id, Destination dest)
+        {
+           if(id != dest.DestinationId)
+           {
+                return BadRequest();
+           }
+           _db.Destinations.Update(dest);
+
+           try
+           {
+               await _db.SaveChangesAsync();
+           }
+           catch (DbUpdateConcurrencyException)
+           {
+                if (!DestinationExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+           }
+           return NoContent();
+        
+        } 
+
+        private bool DestinationExists(int id)
+        {
+            return _db.Destinations.Any(dest => dest.DestinationId == id);
+        }
        
 }
