9d3946dfaeba9609b162a1f8498a464f7db00990
Emzzy241 <emzzyoluwole@gmail.com>
2024-09-22 17:10:13 +0100

    fixing api bugs

---
+++ TravelApi/Migrations/20240922070533_Updating_the_database.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TravelApi.Migrations
+{
+    public partial class Updating_the_database : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
